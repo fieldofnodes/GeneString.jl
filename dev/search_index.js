476abfeaffcdc452a3dafac83e9afe41541ba43b
Documenter.jl <documenter@juliadocs.github.io>
2024-11-28 14:48:46 +0000

    build based on 7e3b29d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GeneString","category":"page"},{"location":"#GeneString","page":"Home","title":"GeneString","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GeneString.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GeneString]","category":"page"}]
+}
